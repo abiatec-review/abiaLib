a9f3d6f813361a423330121e6cbb228c037d466f
Kostya Gorodnichuk <kanstantsin.ha@abiatec.com>
2022-04-29 16:25:35 +0300

    change: logo, theme colors

---
+++ .storybook/abiaTheme.js
@@ -0,0 +1,11 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Abia-lib storybook',
+  brandUrl: 'https://abiatec.by',
+  brandImage: 'https://abiatec.by/media/img/svg/logo-abiatec.svg',
+  colorSecondary: 'rgba(213, 34, 43, .6)',
+  appContentBg: "white",
+  inputBg: "rgba(213, 34, 43, .1)",
+});
