4549575748a773ecf4b58e941807434453188745
ilia78568 <ilia19.99@mail.ru>
2022-05-18 15:54:47 +0300

    fix: add imports for new components

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import Button from 'components/Button';
 import ProgressBar from './components/ProgressBars/ProgressBar';
-
+import ProgressBarCircle from './components/ProgressBars/ProgressBarCircle';
+import ProgressBarSwitch from './components/ProgressBars/ProgressBarSwitch';
+import Switch from './components/Switch';
 
 export {
   Button,
   ProgressBar,
+  ProgressBarCircle,
+  ProgressBarSwitch,
+  Switch
 };
